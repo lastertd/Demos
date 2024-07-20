03f6274e9d648043383ca8037c5d71445600abe0
tongan.liusheng <tongan.liusheng@appshahe.com>
2024-07-20 23:07:03 +0800

    dev: 修改nuxt配置项

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  pages: true,
-  compatibilityDate: "2024-04-03",
   devtools: {enabled: false},
+  pages: true,
+  ssr: true,
   modules: [
     "@nuxtjs/tailwindcss",
   ],
-  ssr:true,
-  nitro: {
-    output: {
-      publicDir: "dist"
-    }
-  }
 });
